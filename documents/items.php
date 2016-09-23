820d293721185157596560aa75bcf73ae30d9592
Maksim.Chugunov <shadeoflance@gmail.com>
2016-09-23 20:10:31 +0300

    Initial commit

---
+++ documents/items.php
@@ -0,0 +1,9 @@
+<?php
+$mysqli = new mysqli("localhost", "root", "", "db");
+$res = $mysqli->query("select * from items");
+if(!$res)
+	echo "error " . $mysqli->errno . ' ' . $mysqli->error;
+while($row = mysqli_fetch_assoc($res)) {
+	echo "${row['id']} ${row['name']} ${row['price']} ${row['description']}</br>";
+}
+?>
